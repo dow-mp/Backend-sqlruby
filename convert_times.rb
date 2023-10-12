fe77c8aa4c100fe4199fe983481cfc92317f3002
dow-mp <derin.mitchell@gmail.com>
2023-10-11 21:04:36 -0400

    fix missing time unit and refactor to break up lrg function

--- convert_times.rb
+++ convert_times.rb
@@ -3,18 +3,26 @@ def years_to_hours
     years = gets.chomp
     hours = years.to_i * 365 * 24
     puts "There are #{hours.to_i} hours in #{years} years."
+end
 
-
+def decades_minutes
     puts("Enter a number of decades.")
     decades = gets.chomp
     minutes = decades.to_i * 10 * 365 * 24 * 60
     puts " There are #{minutes.to_i} minutes in #{decades} decades"
+end
 
-
+def years_to_seconds
     puts("Enter your age.")
     age = gets.chomp
     seconds = age.to_i * 365 * 24 * 60 * 60
-    puts "You are #{seconds.to_i} old."
+    puts "You are #{seconds.to_i} seconds old."
 end
 
-years_to_hours
+def convert_times
+    years_to_hours
+    decades_minutes
+    years_to_seconds
+end
+
+convert_times
